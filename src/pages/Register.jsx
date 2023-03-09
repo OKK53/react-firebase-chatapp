a1cbb296b2dd94191aa363047bc5db3a8482ef02
OKK <OKK.OguzKagan@gmail.com>
2023-03-09 09:28:43 +0300

    first commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,83 @@
+import AddAvatar from "../assets/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
+
+function Register() {
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setError(true);
+          }
+        });
+      });
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">React Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" id="file" className="file" />
+          <label htmlFor="file">
+            <img src={AddAvatar} alt="addAvatar" />
+            <span>Add an Avatar</span>
+          </label>
+          <button>Sign up</button>
+          {error && <span>Something went wrong!</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
